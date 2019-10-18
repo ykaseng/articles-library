6427ebb8ee9e26d5e16cde603a52ef3caacce251
Yang Ka Seng <yang.ka.seng@vinda.com>
2019-10-18 17:07:28 +0800

    Modify ArticleStore to utilize go-pg more.

--- database/articleStore.go
+++ database/articleStore.go
@@ -2,13 +2,14 @@ package database
 
 import (
 	"github.com/go-pg/pg"
+	"github.com/go-pg/pg/orm"
 
 	"github.com/ykaseng/articles-library/models"
 )
 
 // ArticleStore implements database operations for article management.
 type ArticleStore struct {
-	db *pg.DB
+	db orm.DB
 }
 
 // NewArticleStore returns an ArticleStore.
@@ -21,26 +22,17 @@ func NewArticleStore(db *pg.DB) *ArticleStore {
 // Get an article by ID.
 func (s *ArticleStore) Get(id int) (*[]models.Article, error) {
 	q := `
-	SELECT ar.title, ar.content, au.name FROM articles ar INNER JOIN authors au ON ar.author_id = au.id WHERE ar.id = $1
+	SELECT ar.id, ar.title, ar.content, au.name AS author FROM articles ar INNER JOIN authors au ON ar.author_id = au.id WHERE ar.id = ?
 	`
 
-	stmt, err := s.db.Prepare(q)
-	if err != nil {
-		return nil, err
-	}
-
-	defer stmt.Close()
-
-	var a models.Article
-	if _, err = stmt.QueryOne(pg.Scan(&a.Title, &a.Content, &a.Author), id); err != nil {
+	var a []models.Article
+	if _, err := s.db.QueryOne(&a, q, id); err != nil {
 		if err != pg.ErrNoRows {
 			return nil, err
 		}
-
-		return nil, nil
 	}
 
-	return &[]models.Article{a}, nil
+	return &a, nil
 }
 
 // GetAll gets all articles.
@@ -54,8 +46,6 @@ func (s *ArticleStore) GetAll() (*[]models.Article, error) {
 		if err != pg.ErrNoRows {
 			return nil, err
 		}
-
-		return nil, nil
 	}
 
 	return &a, nil
@@ -64,18 +54,11 @@ func (s *ArticleStore) GetAll() (*[]models.Article, error) {
 // Post inserts an article into the database and returns the last insert id.
 func (s *ArticleStore) Post(article *models.Article) (*models.ArticleID, error) {
 	q := `
-		WITH author AS (INSERT INTO authors(name) VALUES ($1) RETURNING id) INSERT INTO articles(title, content, author_id) VALUES($2, $3, (SELECT author.id FROM author)) RETURNING id
+		WITH author AS (INSERT INTO authors(name) VALUES (?) RETURNING id) INSERT INTO articles(title, content, author_id) VALUES(?, ?, (SELECT author.id FROM author)) RETURNING id
 	`
 
-	stmt, err := s.db.Prepare(q)
-	if err != nil {
-		return nil, err
-	}
-
-	defer stmt.Close()
-
 	var articleID models.ArticleID
-	if _, err := stmt.QueryOne(pg.Scan(&articleID.ID), article.Author, article.Title, article.Content); err != nil {
+	if _, err := s.db.QueryOne(&articleID.ID, q, article.Author, article.Title, article.Content); err != nil {
 		return nil, err
 	}
 
